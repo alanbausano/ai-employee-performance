8281662ed7ebddf0d17663391070e8986e4e302d
Alan Bausano <alan.bausano@gmail.com>
2026-03-11 01:28:39 -0300

    modified repeated api file

--- client/fe/src/api/axios.ts
+++ client/fe/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.PROD ? '/api' : 'http://localhost:4000',
+  baseURL: import.meta.env.PROD ? '' : 'http://localhost:4000',
   headers: { 'Content-Type': 'application/json' },
   timeout: 12000,
 });
